2dd7efd5ad15ba3cb8734155a44bf537e6f8d558
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2023-06-29 17:44:18 +0200

    SEO and code highlight updated :sparkles:
    - Added head meta and links
    
    - Code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,63 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
-})
+    content: {
+        highlight: 'monokai',
+    },
+    app: {
+        head: {
+            link: [{ rel: 'icon', href: '/favicon.ico' }],
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+                {
+                    name: 'description',
+                    content:
+                        'Hire me to create an amazing landing page or an awesome blog for your brand. Contact me to write a blog post on any technical topic for your brand.',
+                },
+                { name: 'robots', content: 'index, follow' },
+                { property: 'og:type', content: 'personal website' },
+                {
+                    property: 'og:title',
+                    content: 'Kirill Inoz • Web Developer & Technical Writer',
+                },
+                {
+                    property: 'og:description',
+                    content:
+                        'Hire me to create an amazing landing page or an awesome blog for your brand. Contact me to write a blog post on any technical topic for your brand.',
+                },
+                {
+                    property: 'og:image',
+                    content: 'https://ikirill.com/images/thumbnail.png',
+                },
+                { property: 'og:url', content: 'https://ikirill.com' },
+                {
+                    property: 'og:site_name',
+                    content: 'Kirill Inoz • Web Developer & Technical Writer',
+                },
+                { property: 'og:locale', content: 'en_US' },
+                { property: 'twitter:card', content: 'summary_large_image' },
+                {
+                    property: 'twitter:title',
+                    content: 'Kirill Inoz • Web Developer & Technical Writer',
+                },
+                {
+                    property: 'twitter:description',
+                    content:
+                        'Hire me to create an amazing landing page or an awesome blog for your brand. Contact me to write a blog post on any technical topic for your brand.',
+                },
+                {
+                    property: 'twitter:image',
+                    content: 'https://ikirill.com/images/thumbnail.png',
+                },
+                { property: 'twitter:url', content: 'https://ikirill.com' },
+                { property: 'twitter:site', content: '@kirillinoz' },
+                { property: 'twitter:creator', content: '@kirillinoz' },
+            ],
+            title: 'Kirill Inoz • Web Developer & Technical Writer',
+        },
+    },
+});
